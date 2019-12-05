a4915fc0d5c19e8ea60ef7f6ca634bd0b5a2bb07
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-12-05 18:49:31 +0100

    done

---
+++ app/models/survey.rb
@@ -0,0 +1,4 @@
+class Survey < ApplicationRecord
+    has_many :questions, dependent: :destroy
+    accepts_nested_attributes_for :questions, allow_destroy: :true, reject_if: proc { |att| att['content'].blank? }
+end
